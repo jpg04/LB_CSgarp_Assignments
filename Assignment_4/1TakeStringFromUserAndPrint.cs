6d2fcb8f191d87382de4e2a466301eafdc0742a5
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-26 18:23:04 +0530

    Program which accept string from user and display difference between Factors and Non factors numbers.

---
+++ Assignment_4/1TakeStringFromUserAndPrint.cs
@@ -0,0 +1,17 @@
+//^ 1.Write Program which accept string from user and display difference between Factors and Non factors numbers.
+
+
+using System;
+
+class TakeStringFromUser
+{
+    static void Main(string[] jp)
+    {
+        string Name;
+
+        System.Console.Write("Enter Name : ");
+        Name = Console.ReadLine();
+        
+        System.Console.WriteLine("Your Name is : "+Name);
+    }
+}
